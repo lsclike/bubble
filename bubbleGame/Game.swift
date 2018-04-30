09642ff0a37e73d427575ec42334af44e139a9e9
Master_Cloud <lsclike2016@hotmail.com>
2018-04-30 21:51:37 +1000

    star game

---
+++ bubbleGame/Game.swift
@@ -0,0 +1,22 @@
+//
+//  Game.swift
+//  bubbleGame
+//
+//  Created by Master_Cloud on 30/4/18.
+//  Copyright © 2018 Master_Cloud. All rights reserved.
+//
+
+import Foundation
+
+class Game{
+    var bubbles = [Bubble]()
+    var numberOfBubbles = 15
+    func bubbleMachine(){
+        for _ in 0..<numberOfBubbles {
+        }
+    }
+    
+    func selectColour() -> String {
+        
+    }
+}
