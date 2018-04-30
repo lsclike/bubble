e8591ab0bffd1e1b132a0e2fb40ebf4f52adbd50
Master_Cloud <lsclike2016@hotmail.com>
2018-04-30 21:12:20 +1000

    first attempt

---
+++ bubbleGame/Bubble.swift
@@ -0,0 +1,22 @@
+//
+//  bubble.swift
+//  bubbleGame
+//
+//  Created by Master_Cloud on 30/4/18.
+//  Copyright © 2018 Master_Cloud. All rights reserved.
+//
+
+import Foundation
+
+struct Bubble {
+    var colour:String
+    var score:Int
+    var colourValue = ["red":1,"pink":2,"green":5,"blue":8,"black":10]
+    
+
+    init(colour:String){
+        self.colour = colour
+        self.score = colourValue["\(colour)"]
+    }
+}
+
---
+++ bubbleGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bubbleGame
+//
+//  Created by Master_Cloud on 30/4/18.
+//  Copyright © 2018 Master_Cloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
